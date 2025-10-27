38294742fc27bc3145cd7fe5fead138dd7bb64b4
Pikalot <pikalot@hotmail.com>
2025-10-27 16:20:25 -0700

    Added Amazon browsing

---
+++ README.md
@@ -0,0 +1,18 @@
+
+** How to setup **
+```
+git clone https://github.com/Pikalot/Web-Robot
+python -m venv venv
+```
+
+* Activate the virtual environment: *
+Windows: venv\Scripts\activate
+Mac/Linux: source venv/bin/activate
+
+You should see (venv) appear at the start of the terminal line.
+
+* Install Playwright *
+```
+pip install playwright
+playwright install
+```
--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 from playwright.sync_api import sync_playwright
 
-with sync_playwright() as p:
-    browser = p.firefox.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://tuananh-hub.vercel.app/")
-    print("✅ Page title:", page.title())
-    browser.close()
+def execute():
+    with sync_playwright() as p:
+        # browser = p.chromium.launch(headless=False)
+        browser = p.firefox.launch(headless=False)
+        page = browser.new_page()
+        
+        # Go to Amazon
+        page.goto("https://www.amazon.com/")
+        print("✅ Go to page title:", page.title())
+        
+        # Click on continue if Amazon prompts for new user
+        cont_button = page.get_by_role("button", name = "Continue Shopping")
+        if cont_button.is_visible():
+            cont_button.click()
+
+        search_field = page.get_by_role("searchbox", name = "Search Amazon")
+        search_field.fill("blue shirt")
+        print("✅ Fill search box:", search_field.input_value())
+        # go_button = page.get_by_role("button", name = "Go")
+        # print(go_button)
+        # go_button.click()
+        # search_field.press()
+        
+        browser.close()
+
+if __name__ == "__main__":
+    execute()
