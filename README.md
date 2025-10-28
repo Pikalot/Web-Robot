185d5b38e9838587c41af9e8822865957156a615
Pikalot <pikalot@hotmail.com>
2025-10-27 21:42:10 -0700

    Completed: requirement 1

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
+## How to setup
 
-** How to setup **
 ```
 git clone https://github.com/Pikalot/Web-Robot
 python -m venv venv
 ```
 
-* Activate the virtual environment: *
-Windows: venv\Scripts\activate
-Mac/Linux: source venv/bin/activate
+**Activate the virtual environment:**
 
-You should see (venv) appear at the start of the terminal line.
+- Windows: venv\Scripts\activate
+- Mac/Linux: source venv/bin/activate
 
-* Install Playwright *
+_You should see (venv) appear at the start of the terminal line._
+
+**Install Playwright**
+
 ```
 pip install playwright
 playwright install
+```
+
+## How to run
+
+```
+python main.py
 ```
--- main.py
+++ main.py
@@ -1,29 +1,60 @@
 from playwright.sync_api import sync_playwright
 
-def execute():
+def run_test():
     with sync_playwright() as p:
         # browser = p.chromium.launch(headless=False)
         browser = p.firefox.launch(headless=False)
         page = browser.new_page()
         
-        # Go to Amazon
-        page.goto("https://www.amazon.com/")
-        print("✅ Go to page title:", page.title())
-        
-        # Click on continue if Amazon prompts for new user
-        cont_button = page.get_by_role("button", name = "Continue Shopping")
-        if cont_button.is_visible():
-            cont_button.click()
+        try:
+            # Go to Amazon
+            page.goto("https://www.amazon.com/")
+            print("➡️  Go to page title:", page.title())
+            
+            # Click on continue if Amazon prompts for new user
+            cont_button = page.get_by_role("button", name = "Continue Shopping")
+            if cont_button.is_visible():
+                cont_button.click()
 
-        search_field = page.get_by_role("searchbox", name = "Search Amazon")
-        search_field.fill("blue shirt")
-        print("✅ Fill search box:", search_field.input_value())
-        # go_button = page.get_by_role("button", name = "Go")
-        # print(go_button)
-        # go_button.click()
-        # search_field.press()
-        
-        browser.close()
+            # Fill in search box
+            # search_field = page.get_by_role("searchbox", name = "Search Amazon")            
+            search_field = page.get_by_label("Search Amazon")
+            # search_field = page.locator("#twotabsearchtextbox") # get by ID
+            search_field.wait_for(state="visible", timeout=3000)
+            
+            if not search_field.is_visible():
+                raise Exception("Search box not visible")
+            
+            search_field.fill("blue shirt")
+            print("➡️  Fill search box:", search_field.input_value())
+            
+            # Click Go button
+            page.click("#nav-search-submit-button")
+            print("➡️  Click Go button")
+            print("====================================================================")
+
+            # Find the first product result, print title and price
+            # page.wait_for_selector("div[data-component-type='s-search-result']", timeout=10000)
+            page.wait_for_selector('div[role="listitem"][data-index="3"]', timeout=3000)
+            item = page.locator('div[role="listitem"][data-index="3"]')
+            title = item.locator("a h2 span").first.inner_text()
+            price = item.locator("span.a-price span.a-offscreen").first.inner_text()
+            print(f"✅ Success! Product {title} found")
+            print(f"✅ Price: {price}")
+            print("====================================================================\n")
+            
+            return True # Search successful
+       
+        except Exception as e:
+            print("❌ Error:", e)
+
+def execute(max_entries = 3):
+    for attempt in range(max_entries):
+        print(f"--- Attempt {attempt + 1} of {max_entries} ---")
+        success = run_test()
+
+        if success:
+            break
 
 if __name__ == "__main__":
-    execute()
+    execute(5)
